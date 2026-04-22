05e52f059158950971f5a211b59f6232afe6bcf2
oughlane <mohammedoughlane72@gmail.com>
2026-04-22 10:31:29 +0100

    semaine3

---
+++ Sem4/Challange2.php
@@ -0,0 +1,91 @@
+<?php
+
+class Logging{
+    private static $messages  = [];
+
+    public static function ajoutermessage($niveau,$message ){
+       self::$messages[] =[
+         "niveau"=>$niveau,
+         "message"=>$message
+       ];
+    }
+
+    public static function messages(){
+        foreach(self::$messages as $message){
+            echo "niveau est : ".$message['niveau'];
+            echo "message est : ".$message['message'];
+        }
+    }
+
+    public static function filterNiveau($niveau){
+        $log =[];
+          foreach(self::$messages as $message){
+            if($message["niveau"] == $niveau){
+                $log[] = $message;
+            }
+          }
+          return $log;
+    }
+
+    public static function  countErrur(){
+        $count = 0;
+        foreach(self::$messages as $message){
+            if($message['niveau'] == 'ERROR'){
+                $count++;
+            }
+        }
+        return $count;
+    }
+
+    public static function vider(){
+        self::$messages = [];
+    }
+
+    public static function exporter(){
+        $msg = "";
+         foreach(self::$messages as $message){
+            $msg .= "niveau  : ".$message['niveau']. "  message  : ".$message['message']."\n";
+         }
+         return $msg;
+    }
+}
+
+
+
+
+
+
+Logging::ajoutermessage("INFO", "application démarré");
+Logging::ajoutermessage("ERROR", "connexion échoué");
+Logging::ajoutermessage("WARNING", "mémoire faible");
+
+Logging::messages();
+
+print_r(Logging::filterNiveau('ERROR'));
+
+echo Logging::countErrur()."\n";
+echo Logging::exporter();
+
+
+
+
+class Compteur 
+{
+    private static $count = 0;
+
+    public function __construct()
+    {
+        self::$count++;
+    }
+
+    public static function getCount()
+    {
+        return self::$count;
+    }
+
+}
+$compteur1= new Compteur();
+$compteur2 = new Compteur();
+$compteur3 = new Compteur();
+
+echo Compteur::getCount();
---
+++ Semaine1/lundi/etap1/script.js
@@ -0,0 +1,16 @@
+
+let mumber = ["yassir", "anas", "said", "samir", "amine", "salah"];
+
+console.log(mumber);
+console.log (mumber[0]);
+console.log (mumber[mumber.length - 1]);
+console.log(mumber.length);
+
+mumber.push("aymane", "hassan");
+
+let suprimer = mumber.pop();
+console.log(suprimer);
+
+mumber.shift();
+
+console.log("List", mumber);
